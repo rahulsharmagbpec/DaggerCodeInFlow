a07551e32752cafc23a85a160f92c43c21ea0971
rahulsharma <rahul.sharma@nobroker.in>
2020-05-01 18:49:30 +0530

    testing commit

--- app/src/main/java/com/rahul/daggercodeinflow/CarComponent.java
+++ app/src/main/java/com/rahul/daggercodeinflow/CarComponent.java
@@ -4,5 +4,6 @@ import dagger.Component;
 
 @Component
 public interface CarComponent {
+
     Car getCar();
 }
--- app/src/main/java/com/rahul/daggercodeinflow/Wheel.java
+++ app/src/main/java/com/rahul/daggercodeinflow/Wheel.java
@@ -3,6 +3,7 @@ package com.rahul.daggercodeinflow;
 import javax.inject.Inject;
 
 public class Wheel {
+
     @Inject
     public Wheel() {
     }
